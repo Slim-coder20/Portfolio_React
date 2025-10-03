f9ccc02ea02382d734cb446345c7b6eaf8d2646e
slim <slimabida21@gmail.com>
2025-10-03 10:06:54 +0200

    importation du useTheme dans le layout avec une ternaire pour le déclenchement du mode dark

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import { useTheme } from '../context/ThemeContext'
 
 export default function Layout() {
-  return (
-    <div className='min-h-screen flex flex-col bg-slate-50 dark:bg-slate-950'>
-        <Nav/>
-    <main className="flex-1">
-        <Outlet/>
-    </main>
-        <Footer/>
+  const { isDark } = useTheme()
 
+  return (
+    <div className={`min-h-screen flex flex-col ${isDark ? 'bg-slate-950 dark:bg-slate-950' : 'bg-slate-50'}`}>
+      <Nav/>
+      <main className="flex-1">
+        <Outlet/>
+      </main>
+      <Footer/>
     </div>
   )
 }
