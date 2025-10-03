66401cd374edc04b4a686683df3d6488fcbe93cb
slim <slimabida21@gmail.com>
2025-10-03 10:02:00 +0200

    Création du ThemeContext pour la mis en place global du mode-dark dans l'application

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const ThemeContext = createContext(); 
+
+export function ThemeProvider({children}) {
+    const [isDark, setIsDark] = useState(false); 
+    useEffect(() => {
+        if(isDark) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+
+    }, [isDark])
+
+
+    return (
+        <ThemeContext.Provider value={{isDark, setIsDark}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useTheme = () => useContext(ThemeContext); 
